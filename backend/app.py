74c917b293a94672885fa417ee563bc0746b22c9
Marco Elste <marco.elste@web.de>
2026-09-09 15:49:14 +0200

    ref: Routes projects
       - Updated app.py: Removed project routes
       - Added base.py: Added const BASE_DIR
       - Added project: Added all project routes

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,5 @@
-from flask import Flask, send_from_directory
-from pathlib import Path
+from flask import Flask
+from project import projects
 
 app = Flask(__name__)
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-SHARKIE_DIR = BASE_DIR / 'frontend/sharkie'
-SHARKIE_DOC_DIR = SHARKIE_DIR / 'docs/html'
-JOIN_DIR = BASE_DIR / 'frontend/join/dist/join/browser'
-
-@app.route('/projects/sharkie/')
-def sharkie_index():
-    return send_from_directory(SHARKIE_DIR, 'index.html')
-
-@app.route('/projects/sharkie/<path:path>')
-def sharkie_files(path):
-    return send_from_directory(SHARKIE_DIR, path)
-
-@app.route('/projects/sharkie/docs/')
-def sharkie_docs_index():
-    return send_from_directory(SHARKIE_DOC_DIR, 'index.html')
-
-@app.route('/projects/sharkie/docs/<path:path>')
-def sharkie_docs_files(path):
-    return send_from_directory(SHARKIE_DOC_DIR, path)
-
-@app.route('/projects/join/')
-def join_index():
-    return send_from_directory(JOIN_DIR, 'index.html')
-
-@app.route('/projects/join/<path:path>')
-def join_data(path):
-    return send_from_directory(JOIN_DIR, path)
+app.register_blueprint(projects)
---
+++ backend/project.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, send_from_directory
+from base import BASE_DIR
+
+projects = Blueprint('projects', __name__)
+SHARKIE_DIR = BASE_DIR / 'frontend/sharkie'
+SHARKIE_DOC_DIR = SHARKIE_DIR / 'docs/html'
+JOIN_DIR = BASE_DIR / 'frontend/join/dist/join/browser'
+
+@projects.route('/projects/sharkie/')
+def sharkie_index():
+    return send_from_directory(SHARKIE_DIR, 'index.html')
+
+@projects.route('/projects/sharkie/<path:path>')
+def sharkie_files(path):
+    return send_from_directory(SHARKIE_DIR, path)
+
+@projects.route('/projects/sharkie/docs/')
+def sharkie_docs_index():
+    return send_from_directory(SHARKIE_DOC_DIR, 'index.html')
+
+@projects.route('/projects/sharkie/docs/<path:path>')
+def sharkie_docs_files(path):
+    return send_from_directory(SHARKIE_DOC_DIR, path)
+
+@projects.route('/projects/join/')
+def join_index():
+    return send_from_directory(JOIN_DIR, 'index.html')
+
+@projects.route('/projects/join/<path:path>')
+def join_data(path):
+    return send_from_directory(JOIN_DIR, path)
